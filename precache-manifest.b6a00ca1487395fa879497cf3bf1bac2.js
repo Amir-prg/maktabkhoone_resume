5507761cf44429211aa76b1bd14cf5448de39ada
Amir <amirtplink@gmail.com>
2020-10-25 14:49:45 +0330

    Updates

---
+++ precache-manifest.b6a00ca1487395fa879497cf3bf1bac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a167482d21defd050d0b8ff34fd9083",
+    "url": "/maktabkhoone_resume/index.html"
+  },
+  {
+    "revision": "dd7b68f695d2e81d2def",
+    "url": "/maktabkhoone_resume/static/css/main.34d3ee00.chunk.css"
+  },
+  {
+    "revision": "c8c266f85430774a484a",
+    "url": "/maktabkhoone_resume/static/js/2.3ea625e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maktabkhoone_resume/static/js/2.3ea625e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd7b68f695d2e81d2def",
+    "url": "/maktabkhoone_resume/static/js/main.bf0ed6df.chunk.js"
+  },
+  {
+    "revision": "d4efe1b1e075be2391d9",
+    "url": "/maktabkhoone_resume/static/js/runtime-main.8ca4ce01.js"
+  }
+]);
